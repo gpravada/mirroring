8067354065d5243ddfd94995d18f38ece563b5b8
Gopi Prasad Ravada <gp.ravada@gmail.com>
2021-04-02 07:27:22 +0000

    Update scripts/gen_ver.py, .gitlab-ci.yml files

--- scripts/gen_ver.py
+++ scripts/gen_ver.py
@@ -57,7 +57,6 @@ def push_version(file_path: str, server: str, token: str, project_id: int, branc
     git_server=str(read_file(server))
     git_token=str(read_file(token))
     git_branch=str(read_file(branch))
-    project_id=read_file(project_id)
     
     gl = gitlab.Gitlab(git_server, private_token=git_token)
     p = gl.projects.get(project_id)
